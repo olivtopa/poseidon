6c6ae59af9748c819507aea4c0140b973c201e1c
Olivier <amoliv_825@hotmail.com>
2021-12-17 11:54:53 +0100

    Initialisation des tests

---
+++ src/test/java/com/olivtopa/poseidon/BidTest.java
@@ -0,0 +1,46 @@
+package com.olivtopa.poseidon;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.olivtopa.poseidon.domain.BidList;
+import com.olivtopa.poseidon.repositories.BidListRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BidTest {
+	@Autowired
+	private BidListRepository bidListRepository;
+
+	@Test
+	public void bidListTest() {
+		BidList bid = new BidList("Account Test", "Type Test", 10d);
+
+		// Save
+		bid = bidListRepository.save(bid);
+		Assert.assertNotNull(bid.getBidListId());
+		Assert.assertEquals(bid.getBidQuantity(), 10d, 10d);
+
+		// Update
+		bid.setBidQuantity(20d);
+		bid = bidListRepository.save(bid);
+		Assert.assertEquals(bid.getBidQuantity(), 20d, 20d);
+
+		// Find
+		List<BidList> listResult = bidListRepository.findAll();
+		Assert.assertTrue(listResult.size() > 0);
+
+		// Delete
+		Integer id = bid.getBidListId();
+		bidListRepository.delete(bid);
+		Optional<BidList> bidList = bidListRepository.findById(id);
+		Assert.assertFalse(bidList.isPresent());
+	}
+}
---
+++ src/test/java/com/olivtopa/poseidon/TradeTest.java
@@ -0,0 +1,46 @@
+package com.olivtopa.poseidon;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.olivtopa.poseidon.domain.Trade;
+import com.olivtopa.poseidon.repositories.TradeRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TradeTest {
+	@Autowired
+	private TradeRepository tradeRepository;
+
+	@Test
+	public void tradeTest() {
+		Trade trade = new Trade("Trade Account", "Type");
+
+		// Save
+		trade = tradeRepository.save(trade);
+		Assert.assertNotNull(trade.getTradeId());
+		Assert.assertTrue(trade.getAccount().equals("Trade Account"));
+
+		// Update
+		trade.setAccount("Trade Account Update");
+		trade = tradeRepository.save(trade);
+		Assert.assertTrue(trade.getAccount().equals("Trade Account Update"));
+
+		// Find
+		List<Trade> listResult = tradeRepository.findAll();
+		Assert.assertTrue(listResult.size() > 0);
+
+		// Delete
+		Integer id = trade.getTradeId();
+		tradeRepository.delete(trade);
+		Optional<Trade> tradeList = tradeRepository.findById(id);
+		Assert.assertFalse(tradeList.isPresent());
+	}
+}
