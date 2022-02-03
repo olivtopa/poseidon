0c3c4002e1d288af64968ca0f2601dca1e5b652f
Olivier <amoliv_825@hotmail.com>
2022-02-03 13:25:03 +0100

    correction de la syntaxe du type de map

--- src/test/java/com/olivtopa/poseidon/FormatToUrlEncoded.java
+++ src/test/java/com/olivtopa/poseidon/FormatToUrlEncoded.java
@@ -13,7 +13,7 @@ public class FormatToUrlEncoded {
 	public static String getUrlEncoded(Object dto) {
 	    ObjectMapper MAPPER = new ObjectMapper();
 	    MAPPER.registerModule(new JavaTimeModule());
-	    Map map = MAPPER.convertValue(dto, Map.class);
+	    Map<?,?> map = MAPPER.convertValue(dto, Map.class);
 
 	    return map.toString()
 	            .replace(", ", "&")
