60a773d1b00a10983f3f0003728e252a0595706f
Olivier <amoliv_825@hotmail.com>
2022-02-09 10:04:04 +0100

    TU password validation

--- src/test/java/com/olivtopa/poseidon/ValidatePasswordTest.java
+++ src/test/java/com/olivtopa/poseidon/ValidatePasswordTest.java
@@ -6,6 +6,7 @@ import javax.validation.ConstraintValidatorContext.ConstraintViolationBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -18,23 +19,25 @@ public class ValidatePasswordTest {
 
 	@Mock
 	ConstraintValidatorContext constraintValidatorContext;
+	@Mock
+	ConstraintViolationBuilder constraintViolationBuilder;
 
 	PasswordConstraintValidator passwordConstraintValidator = new PasswordConstraintValidator();
 
 	@Test
-	public void validateGoodPassword() {
+	public void goodPassword() {
 		// Given
 		User user = new User();
 		user.setUsername("Oliv");
 		user.setFullname("toto");
 		user.setRole("role1");
 		user.setPassword("Maison2@");
-				// When + Then
+		// When + Then
 		Assertions.assertTrue(passwordConstraintValidator.isValid(user.getPassword(), constraintValidatorContext));
 	}
 
 	@Test
-	public void validateTooShortPassword() {
+	public void tooShortPassword() {
 
 		// Given
 		User user = new User();
@@ -42,9 +45,66 @@ public class ValidatePasswordTest {
 		user.setFullname("toto");
 		user.setRole("role1");
 		user.setPassword("Maiso@");
-		//Mockito.when(constraintValidatorContext.buildConstraintViolationWithTemplate("")).thenReturn(null);
+		Mockito.when(constraintValidatorContext.buildConstraintViolationWithTemplate(ArgumentMatchers.anyString()))
+				.thenReturn(constraintViolationBuilder);
 		// When + Then
 		Assertions.assertFalse(passwordConstraintValidator.isValid(user.getPassword(), constraintValidatorContext));
 	}
+	
+	@Test
+	public void noSpecialCaractPassword() {
 
+		// Given
+		User user = new User();
+		user.setUsername("Oliv");
+		user.setFullname("toto");
+		user.setRole("role1");
+		user.setPassword("Maison21");
+		Mockito.when(constraintValidatorContext.buildConstraintViolationWithTemplate(ArgumentMatchers.anyString()))
+				.thenReturn(constraintViolationBuilder);
+		// When + Then
+		Assertions.assertFalse(passwordConstraintValidator.isValid(user.getPassword(), constraintValidatorContext));
+	}
+	@Test
+	public void noDigitCaractPassword() {
+
+		// Given
+		User user = new User();
+		user.setUsername("Oliv");
+		user.setFullname("toto");
+		user.setRole("role1");
+		user.setPassword("Maisson@");
+		Mockito.when(constraintValidatorContext.buildConstraintViolationWithTemplate(ArgumentMatchers.anyString()))
+				.thenReturn(constraintViolationBuilder);
+		// When + Then
+		Assertions.assertFalse(passwordConstraintValidator.isValid(user.getPassword(), constraintValidatorContext));
+	}
+	@Test
+	public void noUpercaseCaractPassword() {
+
+		// Given
+		User user = new User();
+		user.setUsername("Oliv");
+		user.setFullname("toto");
+		user.setRole("role1");
+		user.setPassword("maison@21");
+		Mockito.when(constraintValidatorContext.buildConstraintViolationWithTemplate(ArgumentMatchers.anyString()))
+				.thenReturn(constraintViolationBuilder);
+		// When + Then
+		Assertions.assertFalse(passwordConstraintValidator.isValid(user.getPassword(), constraintValidatorContext));
+	}
+	@Test
+	public void noLetterCaractPassword() {
+
+		// Given
+		User user = new User();
+		user.setUsername("Oliv");
+		user.setFullname("toto");
+		user.setRole("role1");
+		user.setPassword("123456789@");
+		Mockito.when(constraintValidatorContext.buildConstraintViolationWithTemplate(ArgumentMatchers.anyString()))
+				.thenReturn(constraintViolationBuilder);
+		// When + Then
+		Assertions.assertFalse(passwordConstraintValidator.isValid(user.getPassword(), constraintValidatorContext));
+	}
 }
